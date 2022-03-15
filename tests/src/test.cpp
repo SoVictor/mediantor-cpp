1613e1d4f6727752d9c0c7738bfdcb4cc0510cea
SoVictor <sovictorcom@yandex.ru>
2022-03-16 02:16:03 +0700

    Make tests parameterized

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -35,62 +35,69 @@ std::vector<fs::path> ListFilesInDirectory(const std::string& directory,
 
 }  // namespace
 
-TEST(TestMediantor, TrivialTest) {
-  const int n = 3;
+class MediantorTest : public ::testing::TestWithParam<Mediantors> {};
+
+TEST_P(MediantorTest, TrivialTest) {
+  const Mediantors mediantor_type = GetParam();
+  cout << endl << "checking " << to_string(mediantor_type) << " ..." << endl;
+
+  const int max_size = 4;
 
   std::unique_ptr<IMediantor> mediantor =
-      std::make_unique<MediantorSortedList>();
+      MakeMediantor(mediantor_type, max_size);
 
   mediantor->Insert(1);
   mediantor->Insert(2);
   mediantor->Insert(3);
+  // Values: {1, 2, 3}.
 
   EXPECT_EQ(2, mediantor->Take());
 
   mediantor->Insert(2);
   mediantor->Insert(4);
+  // Values: {1, 2, 3, 4}.
 
   EXPECT_EQ(2, mediantor->Take());
 }
 
-TEST(TestMediantor, TestOnData) {
-  for (int mediantor_type_idx = 0;
-       mediantor_type_idx < static_cast<int>(Mediantors::kMediantorsNumber);
-       ++mediantor_type_idx) {
-    Mediantors mediantor_type = static_cast<Mediantors>(mediantor_type_idx);
-    cout << endl;
-    cout << "checking " << to_string(mediantor_type) << " ..." << endl;
+TEST_P(MediantorTest, TestOnData) {
+  const Mediantors mediantor_type = GetParam();
+  cout << endl << "checking " << to_string(mediantor_type) << " ..." << endl;
 
-    for (const fs::path& input_path : ListFilesInDirectory("./data/", ".in")) {
-      const std::string test_name = input_path.stem().string();
-      cout << "checking test " << test_name << " ..." << endl;
+  for (const fs::path& input_path : ListFilesInDirectory("./data/", ".in")) {
+    const std::string test_name = input_path.stem().string();
+    cout << "checking test " << test_name << " ..." << endl;
 
-      fs::path ans_path = input_path;
-      ans_path.replace_extension(".out");
+    fs::path ans_path = input_path;
+    ans_path.replace_extension(".out");
 
-      std::ifstream input(input_path);
-      std::ifstream ans(ans_path);
+    std::ifstream input(input_path);
+    std::ifstream ans(ans_path);
 
-      int n;
-      input >> n;
+    int n;
+    input >> n;
 
-      std::unique_ptr<IMediantor> mediantor = MakeMediantor(mediantor_type, n);
+    std::unique_ptr<IMediantor> mediantor = MakeMediantor(mediantor_type, n);
 
-      for (int i = 0; i < n; i++) {
-        int operation;
-        input >> operation;
-        if (operation) {
-          int x;
-          input >> x;
-          mediantor->Insert(x);
-        } else {
-          int x;
-          ans >> x;
-          EXPECT_EQ(x, mediantor->Take());
-        }
+    for (int i = 0; i < n; i++) {
+      int operation;
+      input >> operation;
+      if (operation) {
+        int x;
+        input >> x;
+        mediantor->Insert(x);
+      } else {
+        int x;
+        ans >> x;
+        EXPECT_EQ(x, mediantor->Take());
       }
-
-      EXPECT_TRUE(ans.peek());
     }
+
+    EXPECT_TRUE(ans.peek());
   }
 }
+
+INSTANTIATE_TEST_SUITE_P(MediantorTests, MediantorTest,
+                         testing::Values(Mediantors::kHeap,
+                                         Mediantors::kSqrtDecomp,
+                                         Mediantors::kSortedList));
